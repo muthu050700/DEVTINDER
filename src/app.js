b7026b05b85a829c822dbc3c6b2db50b7f4fb99b
Muthu kumaran <muthumuthu7699@gmail.com>
2025-12-10 05:36:00 +0600

    Explored the Http methods and route

--- src/app.js
+++ src/app.js
@@ -2,13 +2,23 @@ const express = require("express");
 
 const app = express();
 
-
-
-app.use("/test", (req, res) => {
-    res.send("Hello from the server for test!")
+app.use("/user", (req, res) => {
+    res.send("THis will be called for all the HTTP Methods")
 })
-app.use("/", (req, res) => {
-    res.send("Hello world")
+
+app.get("/user", (req, res) => {
+    res.send({
+        firstName: "Muthu",
+        lastName: "Kumar"
+    })
+})
+
+app.post("/user", (req, res) => {
+    res.send("Data saved successgully to the database");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("User deleted successfully")
 })
 
 app.listen(7777, () => {
