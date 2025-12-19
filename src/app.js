866164baeb65b3952a8460b0cb3c199eb4c32deb
Muthu kumaran <muthumuthu7699@gmail.com>
2025-12-19 06:25:12 +0600

    created a admin route, user route

--- src/app.js
+++ src/app.js
@@ -1,26 +1,22 @@
 const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 
-app.use("/user", (req, res) => {
-    res.send("THis will be called for all the HTTP Methods")
-})
+app.use("/admin", adminAuth);
 
-app.get("/user", (req, res) => {
-    res.send({
-        firstName: "Muthu",
-        lastName: "Kumar"
-    })
-})
+app.get("/admin/getAllData", (req, res) => {
+    res.send("All data for the admin");
+});
 
-app.post("/user", (req, res) => {
-    res.send("Data saved successgully to the database");
-})
+app.delete("/admin/deleteData", (req, res) => {
+    res.send("Data deleted successfully");
+});
 
-app.delete("/user", (req, res) => {
-    res.send("User deleted successfully")
+app.get("/user/getUserData", userAuth, (req, res) => {
+    res.send("User Data");
 })
 
 app.listen(7777, () => {
     console.log("Server is successfully listening on port ", 7777);
-})
+});
